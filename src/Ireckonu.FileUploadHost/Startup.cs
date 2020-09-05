6526041bd4524fee5af70c5f6b963f4227a1dd54
iivchenko <iivchenko@live.com>
2020-09-05 15:53:16 +0200

    Add Swagger support to Upload Host

--- src/Ireckonu.FileUploadHost/Startup.cs
+++ src/Ireckonu.FileUploadHost/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Ireckonu.FileUploadHost
 {
@@ -18,6 +19,14 @@ namespace Ireckonu.FileUploadHost
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Ireckonu File Upload API"
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,6 +37,13 @@ namespace Ireckonu.FileUploadHost
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ireckonu File Upload Api V1");
+                c.DocumentTitle = "Ireckonu File Upload";
+            });
 
             app.UseRouting();
 

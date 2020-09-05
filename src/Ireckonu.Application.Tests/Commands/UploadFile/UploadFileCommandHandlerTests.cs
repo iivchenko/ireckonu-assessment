f7a5a41ec28a49f7755a2bef4cdb40e660ce9a09
iivchenko <iivchenko@live.com>
2020-09-05 17:22:52 +0200

    Implement Upload File Command

--- src/Ireckonu.Application.Tests/Commands/UploadFile/UploadFileCommandHandlerTests.cs
+++ src/Ireckonu.Application.Tests/Commands/UploadFile/UploadFileCommandHandlerTests.cs
@@ -1,8 +1,10 @@
 ﻿using Ireckonu.Application.Commands.UploadFile;
 using Ireckonu.Application.Services.FileStorage;
 using Moq;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,14 +18,16 @@ namespace Ireckonu.Application.Tests.Commands.UploadFile
 
         private Mock<IFileStorage> _fileStorage;
         private Mock<IFileFactory> _fileFactory;
+        private Mock<ILogger<UploadFileCommandHandler>> _logger;
  
         [SetUp]
         public void Setup()
         {
             _fileStorage = new Mock<IFileStorage>();
             _fileFactory = new Mock<IFileFactory>();
+            _logger = new Mock<ILogger<UploadFileCommandHandler>>();
 
-            _handler = new UploadFileCommandHandler(_fileStorage.Object, _fileFactory.Object);
+            _handler = new UploadFileCommandHandler(_fileStorage.Object, _fileFactory.Object, _logger.Object);
         }
 
         [Test]
@@ -31,6 +35,7 @@ namespace Ireckonu.Application.Tests.Commands.UploadFile
         {
             // Arrange
             var content = new MemoryStream();
+            var reader = new Mock<IFileReader>();
             var writer = new Mock<IFileWriter>();
 
             var command = new UploadFileCommand
@@ -38,6 +43,14 @@ namespace Ireckonu.Application.Tests.Commands.UploadFile
                 SourceFileName = "File",
                 Content = content
             };
+
+            reader
+                .Setup(x => x.ReadAsync())
+                .ReturnsAsync(Enumerable.Empty<string>());
+
+            _fileFactory
+                .Setup(x => x.CreateReader(content))
+                .Returns(reader.Object);
 
             _fileStorage
                 .Setup(x => x.CreateFile(It.IsAny<string>()))
@@ -52,6 +65,7 @@ namespace Ireckonu.Application.Tests.Commands.UploadFile
             _fileFactory.Verify(x => x.CreateReader(content), Times.Once);
             _fileStorage.Verify(x => x.CreateFile(It.IsAny<string>()), Times.Once);
 
+            reader.Verify(x => x.ReadAsync(), Times.Once);
             writer.Verify(x => x.WriteAsync(It.IsAny<IEnumerable<string>>()), Times.Once);
         }
     }
--- src/Ireckonu.Application/Commands/UploadFile/UploadFileCommandHandler.cs
+++ src/Ireckonu.Application/Commands/UploadFile/UploadFileCommandHandler.cs
@@ -1,7 +1,9 @@
 ﻿using Ireckonu.Application.Services.FileStorage;
 using MediatR;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Ireckonu.Application.Commands.UploadFile
 {
@@ -9,21 +11,36 @@ namespace Ireckonu.Application.Commands.UploadFile
     {
         private readonly IFileStorage _storage;
         private readonly IFileFactory _fileFactory;
+        private readonly ILogger _logger;
 
-        public UploadFileCommandHandler(IFileStorage storage, IFileFactory fileFactory)
+        public UploadFileCommandHandler(
+            IFileStorage storage,
+            IFileFactory fileFactory,
+            ILogger<UploadFileCommandHandler> logger)
         {
             _storage = storage;
             _fileFactory = fileFactory;
+            _logger = logger;
         }
 
-        public Task<UploadFileCommandResponse> Handle(UploadFileCommand request, CancellationToken cancellationToken)
+        public async Task<UploadFileCommandResponse> Handle(UploadFileCommand command, CancellationToken cancellationToken)
         {
-            // Create reader for input file stream
-            // Create target file writer
-            // read/write files
-            // dispose stuff
-            // log stuff
-            throw new System.NotImplementedException();
+            var name = $"{Guid.NewGuid()}-{command.SourceFileName}";
+
+            _logger.LogInformation($"Start file '{command.SourceFileName}' upload into storage as '{name}'");
+
+            using (var reader = _fileFactory.CreateReader(command.Content))
+            using (var writer = _storage.CreateFile(name))
+            {
+                await writer.WriteAsync(await reader.ReadAsync());
+            }
+
+            _logger.LogInformation($"Finish file '{command.SourceFileName}' upload into storage as '{name}'");
+
+            return new UploadFileCommandResponse
+            {
+                TargetFileName = name
+            };
         }
     }
 }

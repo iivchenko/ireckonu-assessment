0d84a4209d0f1455a8e1088f50bd43cb4ba4e6ba
iivchenko <iivchenko@live.com>
2020-09-06 11:26:17 +0200

    Define EF product repository and CVS to Product converter

---
+++ src/Ireckonu.Infrastructure/Domain/ProductAggregate/EfProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Ireckonu.Application.Domain.Common;
+using Ireckonu.Application.Domain.ProductAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Ireckonu.Infrastructure.Domain.ProductAggregate
+{
+    public sealed class EfProductRepository : IRepository<Product, Guid>
+    {
+        public Task CreateAsync(Product aggregate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> FindAsync(Expression<Func<Product, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Product aggregate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Ireckonu.Infrastructure/Services/Converters/CvsToProductConverter.cs
@@ -0,0 +1,17 @@
+﻿using Ireckonu.Application.Domain.ProductAggregate;
+using Ireckonu.Application.Services.Converters;
+using Ireckonu.Application.Services.FileStorage;
+using System.Collections.Generic;
+
+namespace Ireckonu.Infrastructure.Services.Converters
+{
+    public sealed class CvsToProductConverter : IConverter<IFileReader, IAsyncEnumerable<Product>>
+    {
+        public IAsyncEnumerable<Product> Convert(IFileReader reader)
+        {
+            // Read first item (HEADER) and skip
+            // Read items by items converting to a Product
+            throw new System.NotImplementedException();
+        }
+    }
+}
